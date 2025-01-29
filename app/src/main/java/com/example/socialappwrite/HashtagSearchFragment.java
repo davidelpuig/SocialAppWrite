d59a46086c9b3f0591692e429597eb76ab107b98
david <david@elpuig.xeill.net>
2025-01-29 15:55:54 +0100

    Titulo de la appbar

--- app/src/main/java/com/example/socialappwrite/HashtagSearchFragment.java
+++ app/src/main/java/com/example/socialappwrite/HashtagSearchFragment.java
@@ -1,11 +1,28 @@
 package com.example.socialappwrite;
 
+import android.os.Bundle;
+import android.view.View;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+
 import java.util.Arrays;
 import java.util.List;
 
 import io.appwrite.Query;
 
 public class HashtagSearchFragment extends HomeFragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = view.getRootView().findViewById(R.id.toolbar);
+
+        toolbar.setTitle("Hashtag: #"+appViewModel.currentHashtag);
+    }
 
     @Override
     protected List<String> postsQuery()
